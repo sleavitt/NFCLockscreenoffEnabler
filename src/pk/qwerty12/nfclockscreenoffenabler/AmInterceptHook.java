3853a929240443f3f39199e902ea78679c32ec08
Mohammad Abu-Garbeyyeh <mohammad7410@gmail.com>
2013-10-23 12:54:00 +0300

    New icon, missing file from previous commmit.

--- src/pk/qwerty12/nfclockscreenoffenabler/AmInterceptHook.java
+++ src/pk/qwerty12/nfclockscreenoffenabler/AmInterceptHook.java
@@ -1,7 +1,20 @@
 package pk.qwerty12.nfclockscreenoffenabler;
 
+import android.content.Intent;
 import de.robv.android.xposed.XC_MethodHook;
 
-public class AmInterceptHook extends XC_MethodHook {
+/* This hook prevents a potential hacker from exploiting the intent
+ * we use internally to unlock the device.
+ */
 
+public class AmInterceptHook extends XC_MethodHook {
+	@Override
+	protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+		Object result = param.getResultOrThrowable();
+		if (result instanceof Intent) {
+			Intent intent = (Intent) result;
+			if (intent != null && Common.INTENT_UNLOCK_DEVICE.equals(intent.getAction()))
+				param.setResult(new Intent(Common.INTENT_UNLOCK_INTERCEPTED));
+		}
+	}
 }
